c895a7ea2d894e974c7c6ef5658bd4e859a8467e
tnclong <jaden@theplant.jp>
2020-03-27 15:00:22 +0800

    optional POSTGRES_EXTENSIONS

--- create-multiple-postgresql-databases.sh
+++ create-multiple-postgresql-databases.sh
@@ -13,10 +13,27 @@ function create_user_and_database() {
 EOSQL
 }
 
+function create_extension_on_database() {
+	local database=$1
+	local extension=$2
+	echo "    Creating extension '$extension'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d $database <<-EOSQL
+	    CREATE EXTENSION IF NOT EXISTS $extension;
+EOSQL
+}
+
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
 		create_user_and_database $db
+
+		if [ ! -z "${POSTGRES_EXTENSIONS-}" ]; then
+			echo "  Extensions creation requested: $db"
+			for ext in $(echo $POSTGRES_EXTENSIONS | tr ',' ' '); do
+				create_extension_on_database $db $ext
+			done
+			echo "  Extensions created"
+		fi
 	done
 	echo "Multiple databases created"
 fi

bcc0f3b4bcd31ded0f3d9ea162f22a3cb0bb6351
Haochuan <hi@hcsu.io>
2025-10-27 15:15:34 +0900

    Set x86 platform

--- push_docker.sh
+++ push_docker.sh
@@ -1,17 +1,17 @@
-docker build -t public.ecr.aws/theplant/postgresmd:13  --build-arg VER=13-3.5 .
+docker build --no-cache --platform linux/amd64 -t public.ecr.aws/theplant/postgresmd:13  --build-arg VER=13-3.5 .
 docker push public.ecr.aws/theplant/postgresmd:13
 
-docker build -t public.ecr.aws/theplant/postgresmd:14  --build-arg VER=14-3.5 .
+docker build --no-cache --platform linux/amd64 -t public.ecr.aws/theplant/postgresmd:14  --build-arg VER=14-3.5 .
 docker push public.ecr.aws/theplant/postgresmd:14
 
-docker build -t public.ecr.aws/theplant/postgresmd:15  --build-arg VER=15-3.5 .
+docker build --no-cache --platform linux/amd64 -t public.ecr.aws/theplant/postgresmd:15  --build-arg VER=15-3.5 .
 docker push public.ecr.aws/theplant/postgresmd:15
 
-docker build -t public.ecr.aws/theplant/postgresmd:16  --build-arg VER=16-3.5 .
+docker build --no-cache --platform linux/amd64 -t public.ecr.aws/theplant/postgresmd:16  --build-arg VER=16-3.5 .
 docker push public.ecr.aws/theplant/postgresmd:16
 
-docker build -t public.ecr.aws/theplant/postgresmd:17  --build-arg VER=17-3.5 .
+docker build --no-cache --platform linux/amd64 -t public.ecr.aws/theplant/postgresmd:17  --build-arg VER=17-3.5 .
 docker push public.ecr.aws/theplant/postgresmd:17
 
-docker build -t public.ecr.aws/theplant/postgresmd:18  --build-arg VER=18-3.6 .
+docker build --no-cache --platform linux/amd64 -t public.ecr.aws/theplant/postgresmd:18  --build-arg VER=18-3.6 .
 docker push public.ecr.aws/theplant/postgresmd:18

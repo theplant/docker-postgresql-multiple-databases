445ed521b739d0e1ddc1b0872d2123efeb99fa65
Jaden Teng <jaden@theplant.jp>
2025-10-27 13:46:31 +0800

    remove deprecated postgresmd:12

--- push_docker.sh
+++ push_docker.sh
@@ -1,6 +1,3 @@
-docker build -t public.ecr.aws/theplant/postgresmd:12  --build-arg VER=12-3.3 .
-docker push public.ecr.aws/theplant/postgresmd:12
-
 docker build -t public.ecr.aws/theplant/postgresmd:13  --build-arg VER=13-3.5 .
 docker push public.ecr.aws/theplant/postgresmd:13
 
